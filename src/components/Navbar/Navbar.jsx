d93ccee3e631bf0c259bf3ab810490a93dff8888
Benjamin-chidera <benjaminchidera72@gmail.com>
2024-04-04 22:33:28 +0100

    fixed: changed the width of the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,46 +14,48 @@ export const Navbar = () => {
 
   return (
     <nav className=" relative">
-      <main className=" flex justify-around items-center h-[60px] md:h-[80px]  w-full fixed top-0 bg-white z-20 ">
-        <div>
-          <Link to={"/"}>
-            {" "}
-            <img
-              src={revenueLogo}
-              className="w-[110px] h-[44px] object-contain md:w-[140px] md:h-[54.22px]"
-              alt="company-logo"
-            />
-          </Link>
-        </div>
+      <main className="bg-white w-full h-[60px] md:h-[80px] flex  fixed top-0  z-20 right-0 left-0">
+        <section className=" flex justify-between items-center  w-11/12 container mx-auto ">
+          <div>
+            <Link to={"/"}>
+              {" "}
+              <img
+                src={revenueLogo}
+                className="w-[110px] h-[44px] object-contain md:w-[140px] md:h-[54.22px]"
+                alt="company-logo"
+              />
+            </Link>
+          </div>
 
-        <div className="hidden md:flex gap-5 items-center font-semibold text-[18px] w-[350px]">
-          <Link to="/" className=" text-blue nav__link">
-            Home
-          </Link>
-          <Link to="/about" className=" text-blue nav__link">
-            What We Do
-          </Link>
-          <Link to="/resource" className=" text-blue nav__link">
-            Resources Hub
-          </Link>
-        </div>
+          <div className="hidden md:flex gap-5 items-center font-semibold text-[18px] w-[350px]">
+            <Link to="/" className=" text-blue nav__link">
+              Home
+            </Link>
+            <Link to="/about" className=" text-blue nav__link">
+              What We Do
+            </Link>
+            <Link to="/resource" className=" text-blue nav__link">
+              Resources Hub
+            </Link>
+          </div>
 
-        <Link
-          to="/login"
-          className=" border border-blue w-[90px] h-[45px] md:flex justify-center items-center rounded-lg text-blue font-semibold hover:bg-blue hover:text-white duration-500 hidden "
-        >
-          Sign In
-        </Link>
+          <Link
+            to="/login"
+            className=" border border-blue w-[90px] h-[45px] md:flex justify-center items-center rounded-lg text-blue font-semibold hover:bg-blue hover:text-white duration-500 hidden "
+          >
+            Sign In
+          </Link>
 
-        <div className="text-blue font-bold md:hidden">
-          <Hamburger toggled={isOpen} toggle={setOpen} size={30} />
-        </div>
+          <div className="text-blue font-bold md:hidden">
+            <Hamburger toggled={isOpen} toggle={setOpen} size={30} />
+          </div>
+        </section>
       </main>
 
       {/* mobile */}
       {isOpen && (
         <motion.section
-          className="bg-white w-[200px] h-full fixed top-0 z-10 right-0"
+          className="bg-white w-full h-full fixed top-0 z-10 right-0"
           initial={{
             x: 200,
           }}
@@ -61,7 +63,7 @@ export const Navbar = () => {
           exit={{ x: 200, transition: { duration: 0.3 } }}
           transition={{ duration: 0.5 }}
         >
-          <ul className="flex pt-20 flex-col justify-center items-center gap-8 font-semibold text-blue text-md">
+          <ul className="flex pt-20 flex-col justify-center items-center gap-8 font-semibold text-blue text-lg">
             <li>
               <Link to="/" onClick={handleClose}>
                 Home
