1d241923d9af75aef8975904eaf5c3e925753a73
aydeetech <ayomidefaseun1267@gmail.com>
2024-04-05 14:47:11 +0100

    feat: worked on admin layout and admin settings page

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -9,9 +9,9 @@ const Adminlayout = () => {
       <div className='block'>
       <AdminSidebar />
       </div>
-      <div className='2xl:w-full 2xl:max-w-full lg:max-w-[1040px]  w-full lg:ml-auto xl:pl-0 lg:pl-0'>
+      <div className='2xl:w-full lg:w-[80%] 2xl:max-w-full lg:max-w-[1040px] container  w-full lg:ml-auto xl:pl-0 lg:pl-0'>
       <AdminNavbar />
-      <div className='px-8 2xl:max-w-full mt-4  lg:max-w-[1000px] pl-7 w-full  container mx-auto'>
+      <div className='px-8 2xl:max-w-full mt-4  lg:max-w-[1000px] w-full container mx-auto'>
 
         <Outlet />
       </div>
