405a2ab38b7a1c851801bce0618ee6254cd3ee37
aydeetech <ayomidefaseun1267@gmail.com>
2024-04-23 12:05:27 +0100

    updated admin-login

--- src/components/admin/dashboard/Courses.jsx
+++ src/components/admin/dashboard/Courses.jsx
@@ -28,42 +28,42 @@ const courseEarn = [
 
 const Courses = () => {
   return (
-    <main className="md:flex items-center justify-between gap-10">
+    <main className="md:flex items-center justify-between gap-1">
       {/* <section> */}
-      <section className="md:flex items-center gap-10">
-        <div className="flex items-center gap-3 shadow shadow-[#032BF2] p-2 rounded-md">
+      <section className="md:flex items-center gap-1">
+        <div className="flex items-center border-[rgba(0,0,0,0.1)] border-2 h-24 w-[280px] gap-3 shadow-xl shadow-[#032BF214] p-2 rounded-md">
           {/* Total Earnings */}
           <img src={course1} alt="" className="w-14 h-14" />
 
           <div>
             <p className=" text-sm text-grey ">Total Earnings</p>
-            <h3 className=" text-blue font-bold text-2xl">
+            <h3 className=" text-blue font-[1000] text-2xl">
               {formatCurrency("362870")}
             </h3>
           </div>
         </div>
 
-        <div className="flex items-center gap-3  shadow shadow-[#032BF2] p-2 rounded-md">
+        <div className="flex items-center h-24 gap-3 w-[280px] border-[rgba(0,0,0,0.1)] border-2  shadow-xl shadow-[#032BF214] p-2 rounded-md">
           {/* Total Clients */}
           <img src={course2} alt="" className="w-14 h-14" />
 
           <div>
             <p className=" text-sm text-grey ">Total Clients</p>
-            <h3 className=" text-blue font-bold text-2xl">7,876</h3>
+            <h3 className=" text-blue font-[1000] text-2xl">7,876</h3>
           </div>
         </div>
-        <div className="flex items-center gap-3  shadow shadow-[#032BF2] p-2 rounded-md">
+        <div className="flex items-center h-24 gap-3 w-[280px]  border-[rgba(0,0,0,0.1)] border-2 shadow-xl shadow-[#032BF214] p-2 rounded-md">
           {/* Total Clients */}
           <img src={course3} alt="" className="w-14 h-14" />
 
           <div>
             <p className=" text-sm text-grey ">Total Clients</p>
-            <h3 className=" text-blue font-bold text-2xl">7,876</h3>
+            <h3 className=" text-blue font-[1000] text-2xl">7,876</h3>
           </div>
         </div>
       </section>
       {/* </section> */}
-      <button className=" bg-blue text-white h-14 w-44 font-semibold rounded-md">
+      <button className=" bg-blue text-white h-24 w-full font-semibold rounded-md">
         + Add a Course
       </button>
     </main>
