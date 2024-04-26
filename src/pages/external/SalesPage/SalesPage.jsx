0f526b090e380653451c2cfd1a9bb17844256f92
Daniel <danielkore1234@gmail.com>
2024-04-26 13:31:11 +0100

    update

--- src/pages/external/SalesPage/SalesPage.jsx
+++ src/pages/external/SalesPage/SalesPage.jsx
@@ -1,8 +1,14 @@
 
+import { useEffect } from "react";
 import HeroSection from "./HeroSection/HeroSection";
 import Section1 from "./Section1/Section1";
+import { useLocation } from "react-router";
 
 const SalesPage = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <div className="pt-20 w-11/12 container mx-auto">
     <HeroSection/>
